e2c6145b75c47ddd8039b7051f63883bfd8bf485
Akhtyamov Pavel <akhtyamovpavel@gmail.com>
2016-09-04 13:19:57 +0300

    Fixed typos and added some prerequisites

--- seminars/sem01_bvt_bagging/get_data.sh
+++ seminars/sem01_bvt_bagging/get_data.sh
@@ -3,7 +3,8 @@
 # ==== Download adult ====
 curl -o ./data/adult.data https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
 
+# No need for this seminar
 # ==== Download cifar10 ====
-mkdir ./data/cifar10
-curl -o ./data/cifar10/cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xvzf ./data/cifar10/cifar-10-python.tar.gz -C ./data/cifar10/
+#mkdir ./data/cifar10
+#curl -o ./data/cifar10/cifar-10-python.tar.gz https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+#tar -xvzf ./data/cifar10/cifar-10-python.tar.gz -C ./data/cifar10/
